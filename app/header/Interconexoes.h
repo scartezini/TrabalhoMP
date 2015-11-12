e6303d1b6fdedcab3c3ad9389c7a8649168157b7
scartezini <140137084@aluno.unb.br>
2015-11-12 18:19:28 -0200

    vai

--- app/header/Interconexoes.h
+++ app/header/Interconexoes.h
@@ -49,8 +49,11 @@ typedef struct interconexao{
 	int posicaoFinal[2];
 	float chanceFalha;
 	int tempoConserto;
+	int capacidadeMaxima;
 	int custoConserto;
 	int numeroFalha;
+	int tagFalha;
+	
 	Interconexao *proximo;
 	Adaptador *adaptador;
 	Cidade *cidade;
