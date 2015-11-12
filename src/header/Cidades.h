3af638463c7604970955a31de5cbb4056656f0dc
scartezini <140137084@aluno.unb.br>
2015-11-11 22:37:45 -0200

    inicio das cidades

--- src/header/Cidades.h
+++ src/header/Cidades.h
@@ -4,7 +4,21 @@ typedef struct cidade{
 	char *nome;
 	int posicao[2];
 	int recursoNecessario;
+	int recursoGasto;
 	
-	Cidades *proximo;
+	Cidade *proximo;
 	
-}Cidades;
+}Cidade;
+
+
+/**
+*	Resultado da soma de todos recursos 
+* gasto pelas cidades 
+*
+*	@param cidade
+*		ponteiro para o inicio da lista de cidades
+*	
+*	@return 
+*		total de recursos gasto pelas cidades
+**/
+int recursoGastoTotal(Cidade *cidade);
