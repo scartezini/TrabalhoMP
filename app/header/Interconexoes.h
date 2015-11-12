28a794f9e6ae8ab30240615803396ccb55cf7784
LSantos06 <lucas@lucas-Inspiron-1545.(none)>
2015-11-12 18:30:35 -0200

    Teste2

--- app/header/Interconexoes.h
+++ app/header/Interconexoes.h
@@ -1,11 +1,9 @@
 #include "Adaptadores.h"
 #include "Cidades.h"
-<<<<<<< HEAD
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
-=======
 
 /**
 * 	Cabecalho do elemento Gerador
