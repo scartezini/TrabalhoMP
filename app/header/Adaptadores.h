41713ca76c965726eca5406800d96e042ca9804d
scartezini <140137084@aluno.unb.br>
2015-11-12 18:19:02 -0200

    adaptadores

--- app/header/Adaptadores.h
+++ app/header/Adaptadores.h
@@ -12,11 +12,32 @@
 *
 * 	interconexao:
 * 		representa as interconexoes realizadas/apontadas pelos adaptadores
+*
+*	peso:
+*		vetor que guarda a porcentagem que cada conexao vai receber
 **/
 
 typedef struct adaptador{
 	char *nome;
 	int posicao[2];
-	Interconexao *interconexao;
+	Interconexao **interconexao;
+	
+	Adaptador *proximo;
+	float *peso;
 	
 }Adaptador;
+
+
+/**
+*	Define como sera a distribuicao entre as conexoes 
+* que esta ligada a cada adaptador e guarda a porcentagem
+* a ser distribuida no vetor do peso
+*
+*	interconexao->capacidadeMax /
+*	soma de todas as capacidadeMax
+*  
+* @param adapatador
+* 		ponteiro para o inico da lista de adaptadores
+*
+**/
+void defineDistribuicao(Adaptador *adapatador);
