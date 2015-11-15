703151ff3c18bfc3423a003d5188af9cc758d332
LSantos06 <lucasnascimentoss@gmail.com>
2015-11-15 16:27:16 -0200

    Assertivas e comentarios da main

--- app/src/Principal.c
+++ app/src/Principal.c
@@ -5,38 +5,56 @@
 
 int main()
 {
-	FILE *fp = fopen("teste.txt","r"); //Abre arquivo de entrada
+	FILE *fp = fopen("teste.txt","r"); //!< Abre arquivo de entrada
 	Cidade *listaCidades = criaListaCidade();
 	Gerador *listaGeradores = criaListaGerador();
 	Interconexao *listaInterconexoes = criaListaInterconexao();
 	Adaptador *listaAdaptadores = criaListaAdaptador();
-	char str[100]; //String auxiliar para obter registros
+	char str[100]; //!< String auxiliar para obter registros
 	
-	do //Percorre o arquivo de entrada
-	{
-		if(fgets(str,100,fp)!=NULL) //Obtem uma linha(um registro) do arquivo de entrada, verificando se o arquivo chegou ao fim
-		{
-			if(str[0] == 'C') //Caso o registro obtido seja do tipo Cidade
+	do{
+	//! AE: o arquivo nao chegou ao fim
+	
+		if(fgets(str,100,fp)!=NULL){
+		//! AE: a linha (registro) obtido do arquivo possui conteudo
+		
+			if(str[0] == 'C'){
+			//! AE: O registro obtido eh do tipo Cidade
 				listaCidades = insereCidade(str,listaCidades);
-			else if(str[0] == 'G') //Caso o registro obtido seja do tipo Gerador
+			} 
+			else if(str[0] == 'G'){
+			//! AE: O registro obtido eh do tipo Gerador 
 				listaGeradores = insereGerador(str,listaGeradores);
-			else if(str[0] == 'I') //Caso o registro obtido seja do tipo Interconexão
+			}
+			else if(str[0] == 'I'){
+			//! AE: O registro obtido eh do tipo Interconexao 
 				listaInterconexoes = insereInterconexao(str,listaInterconexoes);
-			else if(str[0] == 'A') //Caso o registro obtido seja do tipo Adaptador
+			}
+			else if(str[0] == 'A'){
+			//! AE: O registro obtido eh do tipo Adaptador 
 				listaAdaptadores = insereAdaptador(str,listaAdaptadores);
+			}
 		}
-	}
-	while(!feof(fp));
+		//! AS: a linha (registro) obtido do arquivo nao possui conteudo, ou seja, o arquivo chegou ao fim
+		
+	} while(!feof(fp));
+	//! AS: o arquivo chegou ao fim
 
 	fclose(fp);
 	
-	//Imprime listas obtidas
+	//! Comentarios de argumentacao
+		/**
+		*	Imprimindo as listas obtidas a partir do arquivo de entrada
+		**/
 	printf("\nlistaCidades:\n");imprimeListaCidade(listaCidades);
 	printf("\nlistaGeradores:\n");imprimeListaGerador(listaGeradores);
 	printf("\nlistaInterconexoes:\n");imprimeListaInterconexao(listaInterconexoes);
 	printf("\nlistaAdaptadores:\n");imprimeListaAdaptador(listaAdaptadores);
-
-	//Desaloca listas obtidas
+	
+	//! Comentarios de argumentacao
+		/**
+		*	Desalocando as listas obtidas
+		**/
 	liberaListaCidade(listaCidades);
 	liberaListaGerador(listaGeradores);
 	liberaListaInterconexao(listaInterconexoes);
