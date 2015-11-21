fc0bc9b8741ae2eee428706769448f2e94f010f6
Lucas Santos <lucasnascimentoss@gmail.com>
2015-11-21 00:40:47 -0200

    Teste de interconexoes e arrumando o Makefile

--- test/Adaptadores_unittest.c
+++ test/Adaptadores_unittest.c
@@ -1,26 +1,28 @@
 #include <gtest/gtest.h>
 #include "../app/header/Adaptadores.h"
 
+//! Testando a funcao criaListaAdaptador
 TEST(criaListaAdaptador, vazia){
 
 	Adaptador *adaptador = criaListaAdaptador();
 	ASSERT_TRUE(adaptador == NULL);
-
 }
 
+//! Testando a funcao adaptadorVazio
+	//! Caso vazio
 TEST(adaptadorVazio, vazio){
 	Adaptador *adaptador = criaListaAdaptador();
 	ASSERT_TRUE(adaptadorVazio(adaptador) == VAZIO);
-
 }
-
+	//! Caso naoVazio
 TEST(adaptadorVazio, naoVazio){
 	Adaptador *adaptador = criaListaAdaptador();
 	adaptador = (Adaptador *) malloc (sizeof(Adaptador));
 	ASSERT_TRUE(adaptadorVazio(adaptador) == NAO_VAZIO);
 }
 
-
+//! Testando a funcao insereAdaptador
+	//! Caso vazio
 TEST(insereAdaptador, Vazio){
 	Adaptador *adaptador = criaListaAdaptador();
 
@@ -32,10 +34,10 @@ TEST(insereAdaptador, Vazio){
 
 	adaptador = insereAdaptador(entrada,adaptador);
 	ASSERT_TRUE(strcmp(nome,adaptador->nome) == 0);
-	ASSERT_TRUE(adaptador->posicao[0] == 2);
-	ASSERT_TRUE(adaptador->posicao[1] == 3);
+	EXPECT_EQ(adaptador->posicao[0], 2);
+	EXPECT_EQ(adaptador->posicao[1], 3);
 }
-
+	//! Caso naoVazio
 TEST(insereAdaptador, naoVazio){
 	Adaptador *adaptador = criaListaAdaptador();
 
@@ -47,24 +49,23 @@ TEST(insereAdaptador, naoVazio){
 
 	adaptador = insereAdaptador(entrada,adaptador);
 	ASSERT_TRUE(strcmp(nome,adaptador->nome) == 0);
-	ASSERT_TRUE(adaptador->posicao[0] == 2);
-	ASSERT_TRUE(adaptador->posicao[1] == 3);
+	EXPECT_EQ(adaptador->posicao[0], 2);
+	EXPECT_EQ(adaptador->posicao[1], 3);
 
+	//! Inicio do teste em si
 	strcpy(entrada,"A adapta_2 4 5");
 	strcpy(nome, "adapta_2");
 
 	adaptador = insereAdaptador(entrada,adaptador);
 	ASSERT_TRUE(strcmp(nome,adaptador->nome) == 0);
-	ASSERT_TRUE(adaptador->posicao[0] == 4);
-	ASSERT_TRUE(adaptador->posicao[1] == 5);
-
+	EXPECT_EQ(adaptador->posicao[0], 4);
+	EXPECT_EQ(adaptador->posicao[1], 5);
 
 	strcpy(entrada,"A adapta_1 2 3");
 	strcpy(nome, "adapta_1");
 
 	adaptador = adaptador->proximo;
 	ASSERT_TRUE(strcmp(nome,adaptador->nome) == 0);
-	ASSERT_TRUE(adaptador->posicao[0] == 2);
-	ASSERT_TRUE(adaptador->posicao[1] == 3);
-
+	EXPECT_EQ(adaptador->posicao[0], 2);
+	EXPECT_EQ(adaptador->posicao[1], 3);
 }
