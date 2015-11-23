59c2f72f788497f011e3aed526a1d090b7ef2741
Lucas Santos <lucasnascimentoss@gmail.com>
2015-11-23 21:36:15 -0200

    Atualizando o HTML do doxygen

--- doxygen/html/_principal_8h.js
+++ doxygen/html/_principal_8h.js
@@ -1,10 +1,5 @@
 var _principal_8h =
 [
-    [ "cidade", "structcidade.html", "structcidade" ],
-    [ "adaptador", "structadaptador.html", "structadaptador" ],
-    [ "interconexao", "structinterconexao.html", "structinterconexao" ],
-    [ "gerador", "structgerador.html", "structgerador" ],
-    [ "relatorio", "structrelatorio.html", "structrelatorio" ],
     [ "Adaptador", "_principal_8h.html#af06b5ec73aa2348deb069304c8884eb9", null ],
     [ "Cidade", "_principal_8h.html#ab3c268b4873edddd71d5e67e30582e7d", null ],
     [ "CondicaoCidade", "_principal_8h.html#af017e6e6ef6d471439f7076382d9a7bc", null ],
@@ -15,25 +10,9 @@ var _principal_8h =
     [ "Relatorio", "_principal_8h.html#a74c9d9bcd5f4bbf56461d97daab221b0", null ],
     [ "Vazia", "_principal_8h.html#a453af0716bcc535c4aa941283edbb273", null ],
     [ "Vazio", "_principal_8h.html#ad14a49393e140b5aaf590f1133a735f3", null ],
-    [ "condicaoCidade", "_principal_8h.html#a385a14573e75e4c37ebcde69926dfc8c", [
-      [ "VERDE", "_principal_8h.html#a385a14573e75e4c37ebcde69926dfc8caf9efd9e6bd17c0580d8a0432ba1b04a2", null ],
-      [ "AMARELO", "_principal_8h.html#a385a14573e75e4c37ebcde69926dfc8ca968fd04270855d4f614b4650f33e5f9f", null ],
-      [ "VERMELHO", "_principal_8h.html#a385a14573e75e4c37ebcde69926dfc8ca2f985094c4ea0066b0e04d47f38d2153", null ]
-    ] ],
-    [ "destino", "_principal_8h.html#a467017c0eacafcaed2939e930c562d0e", [
-      [ "ADAPTADOR", "_principal_8h.html#a467017c0eacafcaed2939e930c562d0eae9841c3bad30a33d494772b8db733870", null ],
-      [ "CIDADE", "_principal_8h.html#a467017c0eacafcaed2939e930c562d0ea1b84583223e4c2e9d5f01c1126d9e94a", null ]
-    ] ],
-    [ "falha", "_principal_8h.html#a78592843de133ea1bf1e49c527aa9e86", [
-      [ "FALHA", "_principal_8h.html#a78592843de133ea1bf1e49c527aa9e86aad016e79b1e6a04be51ee73d7a5be62f", null ],
-      [ "SEM_FALHA", "_principal_8h.html#a78592843de133ea1bf1e49c527aa9e86a10130aa3a2fc81c7bab44f43d1ddd929", null ]
-    ] ],
-    [ "vazia", "_principal_8h.html#aed90cefa1d5654e6330aa65575ed3650", [
-      [ "VAZIA", "_principal_8h.html#aed90cefa1d5654e6330aa65575ed3650a7178e5f8c6909b8396c7bcfe8c6549fc", null ],
-      [ "NAO_VAZIA", "_principal_8h.html#aed90cefa1d5654e6330aa65575ed3650aed5ec5baeae83fe6a99581cd31ac7238", null ]
-    ] ],
-    [ "vazio", "_principal_8h.html#acd5b0239b6ddb26b60a72b3329ff4059", [
-      [ "VAZIO", "_principal_8h.html#acd5b0239b6ddb26b60a72b3329ff4059abc711f491a56ec7af7a688be508c5113", null ],
-      [ "NAO_VAZIO", "_principal_8h.html#acd5b0239b6ddb26b60a72b3329ff4059a6a85ddb601ea3b6bee0f8d8361b162ef", null ]
-    ] ]
+    [ "condicaoCidade", "_principal_8h.html#a385a14573e75e4c37ebcde69926dfc8c", null ],
+    [ "destino", "_principal_8h.html#a467017c0eacafcaed2939e930c562d0e", null ],
+    [ "falha", "_principal_8h.html#a78592843de133ea1bf1e49c527aa9e86", null ],
+    [ "vazia", "_principal_8h.html#aed90cefa1d5654e6330aa65575ed3650", null ],
+    [ "vazio", "_principal_8h.html#acd5b0239b6ddb26b60a72b3329ff4059", null ]
 ];
